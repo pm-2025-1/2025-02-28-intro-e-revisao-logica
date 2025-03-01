86c30a3635f43b547f619dcdaebc74b0e18952df
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2025-03-01 15:19:07 -0300

    Add files via upload

---
+++ src/teste.js
@@ -0,0 +1,43 @@
+console.log("teste")
+console.log("aula")
+var x = 5
+// nomeCompleto camel case
+// NomeCompleto pascal case
+// nome_completo snake case
+var nomeCompleto = 'Marcio Torres'
+console.log(x)
+console.log(typeof(x))
+console.log(1.1 + 1.1 + 1.1)
+x = 'TADS'
+console.log(x)
+console.log(typeof(x))
+x = x + "SSSS"
+console.log(x)
+
+var a = '10'
+var b = 2
+console.log(a + b)
+console.log(b + a)
+console.log(a - b)
+
+if (b > 0) {
+    console.log('b é maior que 0: ' + b)
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log('i é ' + i)
+    if (i == 8) {
+        break
+    }
+}
+
+var u = 9
+while (u > 0) {
+    console.log('maior')
+    u--
+}
+            // 0   1   2   3   4
+var idades = [22, 48, 24, 36, 65]
+console.log(idades[2])
+console.log(idades[0])
+console.log(idades[idades.length - 1])
